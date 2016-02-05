46ef7c1a3151d93efae689016065dfd4151cf29f
SpiderBall <stuart.savannah916@gmail.com>
2016-02-05 15:30:20 +0000

    almost done, some issues with socket library though

--- socket.py
+++ socket.py
@@ -1,40 +1,43 @@
 #import socket module
 from socket import *
 
-serverSocket = socket(AP_INET, SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #prepare a server socket
 
 #Fill in start===================================
-
+serverPort = 12000
+serverSocket.bid('',serverPort)
+serverSocket.listen(1)
+print 'The server is ready to receive'
 #Fill in end=====================================
 
 while True:
 	#Establish the connection
 	print 'Ready to serve...'
-	connectionSocket, addr = #Fill in start->		# <-Fill in end
+	connectionSocket, addr = serverSocket.accept()
 	try: 
-		message = #Fill in start		#<-Fill in end
+		message = connectionSocket.recv(1024)
 		filename = message.split()[1]
 		f = open(filename[1:])
-		outputdata = #Fill in start->		#<-Fill in end
+		outputdata = f.read()#Fill in start->		#<-Fill in end
 
 		#Send one HTTP line into socket
-
-		#fill in start=======================
-		#Fill in end=========================
+	 	connectionSocket.send(outputdata[0])
 
 		#Send the content of the requested file to the client
-		for i in range(0, len(outputdata[i])
+		for i in range(0, len(outputdata[i])):
 			connectionSocket.send(outputdata[i])
 		connectionSocket.close()
 	except IOError:
 		#Send response message for file not found
 		#Fill in start======================
+		print "File not found"
 		#Fill in end========================
 		
 		#Close client socket
 
 		#FIll in start======================
+		connectionSocket.close()
 		#Fill in end========================
 		serverSocket.close()
